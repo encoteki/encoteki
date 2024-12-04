0cb20a07f55780489df5c7e537cbc89e5eb33ef2
ndrewantonio <ndrewantonio@outlook.com>
2024-12-05 06:55:27 +0700

    feat(Home): Create Benefit Section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,27 @@
 import NavBar from '@/components/navbar'
 import Hero from './hero'
+import NFTCollection from './nftCollection'
+import Benefit from './benefit'
 
 export default function Home() {
   return (
-    <main className="relative">
+    <>
       <NavBar />
       <Hero />
-    </main>
+
+      <main className="h-auto bg-white">
+        <div className="relative z-10 mx-auto w-[calc(100%-32px)] tablet:w-[calc(100%-64px)] desktop:max-w-[1440px]">
+          <section
+            id="collection"
+            className="pb-10 pt-20 tablet:pb-[60px] tablet:pt-[120px]"
+          >
+            <NFTCollection />
+          </section>
+          <section id="benefit" className="py-10 tablet:py-[60px]">
+            <Benefit />
+          </section>
+        </div>
+      </main>
+    </>
   )
 }
