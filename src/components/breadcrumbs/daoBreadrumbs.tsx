7cbc7a3cff841a49b4179f80b4c07cd930182335
ndrewantonio <ndrewantonio@outlook.com>
2025-03-21 15:46:10 +0700

    feat: Improve breadcrumbs of DAO

--- src/components/breadcrumbs/daoBreadrumbs.tsx
+++ src/components/breadcrumbs/daoBreadrumbs.tsx
@@ -12,13 +12,19 @@ export default function DAOBreadcrumb({ items }: { items: Breadcrumbs[] }) {
       {items.map((item, index) => {
         return (
           <div key={index} className="flex gap-2 text-lg">
-            <Link href={item.link}>
-              <span
-                className={`transition-colors duration-300 hover:text-neutral-10 ${index != items.length - 1 ? 'text-neutral-40' : 'text-neutral-10'} `}
-              >
-                {item.page}
-              </span>
-            </Link>
+            {index === items.length - 1 ? (
+              <span className={'text-neutral-10'}>{item.page}</span>
+            ) : (
+              <Link href={item.link}>
+                <span
+                  className={
+                    'text-neutral-40 transition-colors duration-300 hover:text-neutral-10'
+                  }
+                >
+                  {item.page}
+                </span>
+              </Link>
+            )}
 
             {index != items.length - 1 && <p className="text-neutral-40">/</p>}
           </div>
